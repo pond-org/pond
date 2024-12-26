581af8750fc607500192ad4227f64e0f92fb694f
Nils Bore <nilsbore@gmail.com>
2024-12-26 21:20:49 +0100

    Got a bit further

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # import pond
 
+import os
 from typing import List, Type, get_args, get_origin
 from dataclasses import dataclass
 
@@ -14,6 +15,25 @@ from parse import parse
 
 def process(value1: float) -> int:
     return int(value1)
+
+
+def get_example_catalog() -> Catalog:
+    catalog = Catalog(
+        drives=[
+            Drive(
+                navigation=[Navigation(dummy=True), Navigation(dummy=False)],
+                images=[1.0, 2.0, 3.0],
+                uncertainty=[0.1, 0.2, 0.3],
+            ),
+            Drive(
+                navigation=[Navigation(dummy=False)],
+                images=[4.0, 5.0],
+                uncertainty=[0.4, 0.5, 0.6],
+            ),
+        ],
+        values=Values(value1=0.5, value2=2, name="One", names=["Two", "Three"]),
+    )
+    return catalog
 
 
 def test_db():
@@ -91,6 +111,16 @@ class TypePath:
                 parts.append(f"[{field.index+1}]")
         return "".join(parts)
 
+    def to_fspath(self) -> os.PathLike:
+        entries = [self.dataset]
+        entries.extend(
+            map(
+                lambda p: p.name if p.index is None else f"{p.name}[{p.index}]",
+                self.path,
+            )
+        )
+        return "/".join(entries)
+
 
 def get_tree_type(path: TypePath, root_type: Type[BaseModel]) -> Type[BaseModel]:
     if not path.path:
@@ -122,6 +152,36 @@ def get_entry(path: str, root_type: Type[BaseModel]) -> BaseModel:
     type_path = TypePath.from_path(path)
     type = get_tree_type(type_path, root_type)
     return get_entry_with_type(type_path, type)
+
+
+# NOTE: this does not require the root_type but we
+# should probably add validation of the type
+# This allows setting paths such as
+# catalog.drives[0].navigation
+# catalog.values
+# catalog.values.value1
+# NOTE: the type could be
+BasicType = str | float | int | bool
+EntryType = BaseModel | List[BaseModel] | BasicType | List[BasicType]
+
+
+def set_entry(path: str, value: EntryType) -> bool:
+    db_path = "test_db"
+    type_path = TypePath.from_path(path)
+    fs_path = type_path.to_fspath()
+    print(f"Writing {fs_path}")
+    schema = get_pyarrow_schema(type(value))
+    table = pa.Table.from_pylist([value.dict()], schema=schema)
+    ds = lance.write_dataset(
+        table, f"{db_path}/{fs_path}.lance", schema=schema, mode="overwrite"
+    )
+
+
+def test_set_entry():
+    catalog = get_example_catalog()
+    set_entry("catalog.values", catalog.values)
+    set_entry("catalog.drives[0].navigation[0]", catalog.drives[0].navigation[0])
+    set_entry("catalog.values.value1", catalog.values.value1)
 
 
 def test_get_entry_with_type():
@@ -161,6 +221,51 @@ def get_type_of_entry(path: str) -> Type[BaseModel]:
     pass
 
 
+def write_dataset():
+    catalog = get_example_catalog()
+    schema = get_pyarrow_schema(Catalog)
+
+    # def producer():
+    #     yield pa.RecordBatch.from_pylist([catalog])
+    data = pa.Table.from_pylist([catalog.dict()], schema=schema)
+
+    ds = lance.write_dataset(
+        data, "test_db/test.lance", schema=schema, mode="overwrite"
+    )
+    return ds
+
+
+def test_append():
+    catalog = Catalog(drives=[], values=None)
+    drives = [
+        Drive(
+            navigation=[Navigation(dummy=True), Navigation(dummy=False)],
+            images=[1.0, 2.0, 3.0],
+            uncertainty=[0.1, 0.2, 0.3],
+        ),
+        Drive(
+            navigation=[Navigation(dummy=False)],
+            images=[4.0, 5.0],
+            uncertainty=[0.4, 0.5, 0.6],
+        ),
+    ]
+    schema = get_pyarrow_schema(Catalog)
+
+    data = pa.Table.from_pylist([catalog.dict()], schema=schema)
+
+    ds = lance.write_dataset(
+        data, "test_db/test.lance", schema=schema, mode="overwrite"
+    )
+    for drive in drives:
+        catalog = Catalog(drives=[drive], values=None)
+        data = pa.Table.from_pylist([catalog.dict()], schema=schema)
+        ds.insert(data)
+
+    print(ds.to_table())
+    catalog = get_entry("test", Catalog)
+    print(catalog)
+
+
 def test_dataset():
     # with pond.Context(Catalog) as ctx:
     #     ctx.add_transform(process, "values.value1", "values.value2")
@@ -170,28 +275,6 @@ def test_dataset():
     # ctx = Context(Catalog)
     # ctx.register_alias(BinNavigation, pd.DataFrame)
     # ctx.add_transform(process, "values.value1", "values.value2")
-    catalog = Catalog(
-        drives=[
-            Drive(
-                navigation=[Navigation(dummy=True), Navigation(dummy=False)],
-                images=[1.0, 2.0, 3.0],
-                uncertainty=[0.1, 0.2, 0.3],
-            ),
-            Drive(
-                navigation=[Navigation(dummy=False)],
-                images=[4.0, 5.0],
-                uncertainty=[0.4, 0.5, 0.6],
-            ),
-        ],
-        values=Values(value1=0.5, value2=2, name="One", names=["Two", "Three"]),
-    )
-    schema = get_pyarrow_schema(Catalog)
-
-    # def producer():
-    #     yield pa.RecordBatch.from_pylist([catalog])
-    data = pa.Table.from_pylist([catalog.dict()], schema=schema)
-
-    ds = lance.write_dataset(data, "./test.lance", schema=schema, mode="overwrite")
     # table = ds.to_table(
     #     # columns={"images": "drives.0.images"}
     #     # columns={"images": "drives.0.images"}
@@ -201,17 +284,7 @@ def test_dataset():
     #     # columns=["values.value2"],
     #     # filter="values.value2 = 3",
     # )
-    table = ds.to_table()
-    print(table.to_pylist()[0])
-    print(Catalog.parse_obj(table.to_pylist()[0]))
-
-    table = ds.to_table(columns=["values"])
-    print(table.to_pylist()[0])
-    print(Values.parse_obj(table.to_pylist()[0]["values"]))
-
-    # scanner = ds.scanner(columns=[])
-    # table = scanner.take([0]) # This raises NotImplementedError
-    # print(table)
+    ds = write_dataset()
 
     for batch in ds.to_batches(columns=["drives"], batch_size=1):
         print(1)
@@ -228,18 +301,14 @@ def test_dataset():
     # )
     # for batch in batches:
     #     print(batch.to_table())
-    table = ds.to_table(columns={"drive": "drives[1]"})
-    print(Drive.parse_obj(table.to_pylist()[0]["drive"]))
-    table = ds.to_table(columns={"drive": "drives[2]"})
-    print(Drive.parse_obj(table.to_pylist()[0]["drive"]))
-    table = ds.to_table(columns={"drive": "drives[1]['navigation'][1]"})
-    print(Navigation.parse_obj(table.to_pylist()[0]["drive"]))
     # print(ds.versions())
 
 
 if __name__ == "__main__":
     # test_db()
     # test_dataset()
-    test_get_entry_with_type()
-    test_get_type()
-    test_get_entry()
+    # test_get_entry_with_type()
+    # test_get_type()
+    # test_get_entry()
+    # test_append()
+    test_set_entry()
