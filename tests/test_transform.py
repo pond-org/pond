b188e47eaa1b536152bbee8cce65d35e04bb9260
Nils Bore <nilsbore@gmail.com>
2025-01-07 21:02:35 +0100

    Transform passing test

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -2,6 +2,7 @@
 
 import os
 from typing import Any
+from functools import partial
 
 import pytest
 
@@ -68,3 +69,37 @@ def test_transform(catalog: Catalog, tmp_path_factory):
     transform()
     lens = Lens(Catalog, "values.value2", db_path=path)
     assert lens.get() == value1_value2(catalog.values.value1)
+
+
+def drive_id(input: Drive) -> Drive:
+    return input
+
+
+def test_list_items(catalog: Catalog, tmp_path_factory):
+    path = tmp_path_factory.mktemp("db")
+    lens = Lens(Catalog, "drives[0]", db_path=path)
+    lens.set(catalog.drives[0])
+    transform = Transform(drive_id, Catalog, "drives[0]", "drives[1]", db_path=path)
+    transform()
+    lens = Lens(Catalog, "drives[1]", db_path=path)
+    assert catalog.drives[0] == lens.get()
+
+
+def nav_list_id(input: list[Navigation]) -> list[Navigation]:
+    return input
+
+
+def test_list(catalog: Catalog, tmp_path_factory):
+    path = tmp_path_factory.mktemp("db")
+    lens = Lens(Catalog, "drives[0].navigation", db_path=path)
+    lens.set(catalog.drives[0].navigation)
+    transform = Transform(
+        nav_list_id,
+        Catalog,
+        "drives[0].navigation",
+        "drives[1].navigation",
+        db_path=path,
+    )
+    transform()
+    lens = Lens(Catalog, "drives[1].navigation", db_path=path)
+    assert catalog.drives[0].navigation == lens.get()
