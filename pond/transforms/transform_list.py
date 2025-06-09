fdc23a5060b8c991d45c5ed2e71ba99b07281e00
Nils Bore <nilsbore@gmail.com>
2025-06-09 21:48:22 +0200

    HUGE simplification of transform list

--- pond/transforms/transform_list.py
+++ pond/transforms/transform_list.py
@@ -13,11 +13,12 @@ from pond.transforms.abstract_transform import (
     AbstractExecuteUnit,
     ExecuteTransform,
 )
+from pond.transforms.transform import Transform
 
 
 # NOTE: this is actually a superset of the functionality
 # in transform, so we could use the same unit tests for that part
-class TransformList(AbstractExecuteTransform):
+class TransformList(Transform):
     def __init__(
         self,
         Catalog: Type[BaseModel],
@@ -25,99 +26,48 @@ class TransformList(AbstractExecuteTransform):
         output: list[str] | str,
         fn: Callable,
     ):
-        self.fn = fn
-        self.inputs = input if isinstance(input, list) else [input]
-        self.outputs = output if isinstance(output, list) else [output]
-
-        types = get_type_hints(self.fn)
-        try:
-            output_types = types.pop("return")
-        except KeyError as e:
-            raise RuntimeError(f"Transform does not have return type!") from e
-        try:
-            if is_subhint(output_types, Tuple):
-                output_types = list(get_args(output_types))
-        except BeartypeDoorNonpepException:
-            pass
-
-        if not isinstance(output_types, list):
-            output_types = [output_types]
-
-        # Here we need to go through and match all of the inputs
-        # and outputs to make sure the list iterations make sense
-        # and that we only have one group to iterate over
-        self.input_lenses = OrderedDict()
-        for (input_name, input_type), i in zip(types.items(), self.inputs):
-            input_lens = LensInfo.from_path(Catalog, i)
+        super().__init__(Catalog, input, output, fn)
+        self.input_inds = []
+        self.output_inds = []
+        wildcard = False
+        for input_lens in self.input_lenses.values():
             try:
                 index = next(
                     index
                     for index, v in enumerate(input_lens.lens_path.path)
                     if v.index == -1
                 )
+                wildcard = True
             except StopIteration:
                 index = -1
-            try:
-                print("Type: ", input_lens.get_type())
-                type_checks = is_subhint(input_lens.get_type(), input_type)
-                assert (
-                    type_checks
-                ), f"Input {input_name} of type {input_type} does not agree with catalog entry {i} with type {input_lens.get_type()}"
-                print(f"{input_lens.get_type()} checks with {input_type}!")
-            except BeartypeDoorNonpepException as m:
-                warnings.warn(str(m))
-            self.input_lenses[i] = (input_lens, index)
-
-        self.output_lenses = OrderedDict()
-        found = False
-        for output_type, o in zip(output_types, self.outputs):
-            output_lens = LensInfo.from_path(Catalog, o)
+            self.input_inds.append(index)
+        if not wildcard:
+            raise ValueError(f"Transform list did not get any inputs with wildcard!")
+        wildcard = False
+        for output_lens in self.output_lenses.values():
             try:
                 index = next(
                     index
                     for index, v in enumerate(output_lens.lens_path.path)
                     if v.index == -1
                 )
-                found = True
+                wildcard = True
             except StopIteration:
                 index = -1
-            try:
-                type_checks = is_subhint(output_lens.get_type(), output_type)
-                assert (
-                    type_checks
-                ), f"Output of type {output_type} does not agree with catalog entry {o} with type {output_lens.get_type()}"
-                print(f"{output_lens.get_type()} checks with {output_type}!")
-            except BeartypeDoorNonpepException as m:
-                warnings.warn(str(m))
-            self.output_lenses[o] = (output_lens, index)
-        assert found, "Node output must have at least one list output with list inputs"
-
-    def get_name(self) -> str:
-        return self.fn.__name__
-
-    def get_docs(self) -> str:
-        return self.fn.__doc__
-
-    def get_fn(self) -> Callable:
-        return self.fn
-
-    def get_inputs(self) -> list[LensPath]:
-        return [i[0].lens_path for i in self.input_lenses.values()]
-
-    def get_outputs(self) -> list[LensPath]:
-        return [o[0].lens_path for o in self.output_lenses.values()]
-
-    def get_transforms(self) -> list[Self]:
-        return [self]
+            self.output_inds.append(index)
+        if not wildcard:
+            raise ValueError(f"Transform list did not get any outputs with wildcard!")
 
     def get_execute_units(self, state: State) -> list[AbstractExecuteUnit]:
         input_lengths = {}
-        for name, (input_lens, index) in self.input_lenses.items():
-            if index == -1:
+        for (name, input_lens), path_index in zip(
+            self.input_lenses.items(), self.input_inds
+        ):
+            if path_index == -1:
                 continue
             i = input_lens.lens_path.clone()
             parent_path = LensPath(
-                i.path[:index] + [TypeField(i.path[index].name, None)]
+                i.path[:path_index] + [TypeField(i.path[path_index].name, None)]
             )
             lens = state.lens(parent_path.to_path())
             if lens.exists():
@@ -125,7 +75,7 @@ class TransformList(AbstractExecuteTransform):
                 continue
             list_index = 0
             while True:
-                i.path[index].index = list_index
+                i.path[path_index].index = list_index
                 lens = state.lens(i.to_path())
                 if not lens.exists():
                     break
@@ -139,38 +89,38 @@ class TransformList(AbstractExecuteTransform):
         ), f"Input lengths are not the same: {input_lengths}"
         length = unique_inputs.pop()
 
-        for o in self.output_lenses.values():
+        for o, path_index in zip(self.output_lenses.values(), self.output_inds):
             # This means we are writing all output values to the same table
-            if o[1] != -1 and o[1] == len(o[0].lens_path.path) - 1:
-                path = o[0].lens_path.path
+            if path_index != -1 and path_index == len(o.lens_path.path) - 1:
+                path = o.lens_path.path
                 parent_path = LensPath(
-                    path[:index] + [TypeField(path[index].name, None)]
+                    path[:path_index] + [TypeField(path[path_index].name, None)]
                 )
                 state[parent_path.to_path()] = []
 
         execute_units = []
         for index in range(0, length):
             inputs = []
-            for i in self.input_lenses.values():
-                if i[1] != -1:
+            for i, path_index in zip(self.input_lenses.values(), self.input_inds):
+                if path_index != -1:
                     print(i)
-                    i[0].set_index(i[1], index)
-                    inputs.append(i[0].lens_path.clone())
-                    i[0].set_index(i[1], -1)
+                    i.set_index(path_index, index)
+                    inputs.append(i.lens_path.clone())
+                    i.set_index(path_index, -1)
                 else:
-                    inputs.append(i[0].lens_path)
+                    inputs.append(i.lens_path)
             # NOTE: setting output indices is actually not strictly necessary
             outputs = []
             append_outputs = []
-            for o in self.output_lenses.values():
-                if o[1] != -1:
-                    o[0].set_index(o[1], index)
-                    outputs.append(o[0].lens_path.clone())
-                    if o[1] == len(o[0].lens_path.path) - 1:
-                        append_outputs.append(o[0].lens_path.clone())
-                    o[0].set_index(o[1], -1)
+            for o, path_index in zip(self.output_lenses.values(), self.output_inds):
+                if path_index != -1:
+                    o.set_index(path_index, index)
+                    outputs.append(o.lens_path.clone())
+                    if path_index == len(o.lens_path.path) - 1:
+                        append_outputs.append(o.lens_path.clone())
+                    o.set_index(path_index, -1)
                 else:
-                    outputs.append(o[0].lens_path)
+                    outputs.append(o.lens_path)
             execute_units.append(
                 ExecuteTransform(
                     inputs=inputs,
